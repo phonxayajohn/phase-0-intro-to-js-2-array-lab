9c31e2f18a8e42cf22a3e3248473566e360babf0
John Phonxaya <phonxayajohn@gmail.com>
2023-05-15 22:09:17 -0700

    intro to array lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+// Assigns initial array to 'cats'
+
+function destructivelyAppendCat(catName) {
+    cats.push(catName);
+}
+
+function destructivelyPrependCat(catName) {
+    cats.unshift(catName);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()  {
+    cats.shift();
+}
+
+function appendCat(catName) {
+    const catsCopy = [...cats];
+    catsCopy.push(catName);
+    return catsCopy;
+}
+
+function prependCat(catName) {
+    const catsCopy = [...cats];
+    catsCopy.unshift(catName);
+    return catsCopy;
+}
+
+function removeLastCat() {
+    const catsCopy = [...cats];
+    catsCopy.pop();
+    return catsCopy;
+}
+
+function removeFirstCat() {
+    const catsCopy = [...cats];
+    catsCopy.shift();
+    return catsCopy;
+}
